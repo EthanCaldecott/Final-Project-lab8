b05958bd8d3fc13309a327fdf1c354f4549353af
EthanCaldecott <64612897+EthanCaldecott@users.noreply.github.com>
2020-05-06 23:31:16 -0700

    Day 2 (Tuesday, May 5)
    Forgot to commit here from Python that why time stamp off

--- Euler-12.py
+++ Euler-12.py
@@ -1,1 +1,48 @@
-#HI comment
+#Ethan Caldecott
+#Final Project
+
+#Project Rosalind : A Rapid Introduction to Molecular Biology (Sudo Code)
+
+Have a string(s)
+A=0
+C=0
+G=0
+T=0
+if A in s:
+    A = A+1
+if C in s:
+    C= C+1
+if G in s:
+    G= G+1
+if T in s:
+    T= T+1
+else 
+  Return A
+  Return C
+  Return G
+  Return T
+
+
+#Project Euler Question 12 (Sudo Code)
+
+they input x which is number of divisors
+
+def final value(x)
+n=0
+input number of divisors(x)
+if x == divisors(triangle number) needed it to test low to hi
+  return triangle number
+else 
+ add 1 to n re-run
+
+def Divisors
+  find math equation for number how to find number of divisor in any number
+
+def Triangle Number(n)
+  triangle number = ((n)(n+1)/2)
+
+Answer return is triangle number
+
+
+
+
