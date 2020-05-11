81a8c840489558b8438bcb2a4369d0560aedd993
EthanCaldecott <64612897+EthanCaldecott@users.noreply.github.com>
2020-05-11 12:31:52 -0700

    Edited Code 1 Project Rosalind Quesiton Complete

--- Euler-12.py
+++ Euler-12.py
@@ -1,35 +1,46 @@
 #Ethan Caldecott
 #Final Project
 
-#Project Rosalind : A Rapid Introduction to Molecular Biology (Sudo Code)
+#Project Rosalind : A Rapid Introduction to Molecular Biology
+"""
+A string is simply an ordered collection of symbols selected from some alphabet and formed into a word; the length of a string is the number of symbols that it contains.
+
+An example of a length 21 DNA string (whose alphabet contains the symbols 'A', 'C', 'G', and 'T') is "ATGCTTCAGAAAGGTCTTACG."
+
+Given: A DNA string s of length at most 1000 nt.
+
+Return: Four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occur in s.
+"""
 
 
-A=0
-C=0
-G=0
-T=0
-for i in MolecularBio: 
-    if i == 'C':
-        C = C + 1
-for i in MolecularBio: 
-    if i == 'C':
-        C = C + 1
-for i in MolecularBio: 
-    if i == 'G':
-        G = G + 1
-for i in MolecularBio: 
-    if i == 'T':
-        T = T + 1
-print ("Number of A's: " + str(A))
-print ("Number of C's: " + str(C))
-print ("Number of G's: " + str(G))
-print ("Number of T's: " + str(T))
+def MolecularBio(s):
+    A=0                   #Base A counter starts at 0
+    C=0                   #C Counter starts at 0
+    G=0                   #G counter starts at 0
+    T=0                   #T counter starts at 0
+    for i in s:           #Add 1 to A counter every time a A is detected in the string
+        if i == 'A':
+            A = A + 1
+    for i in s:           #Add 1 to C counter every time a C is detected in the string
+        if i == 'C':
+            C = C + 1
+    for i in s:           #Add 1 to G counter every time a G is detected in the string
+        if i == 'G':
+            G = G + 1       
+    for i in s:           #Add 1 to T counter every time a T is detected in the string              
+        if i == 'T':
+            T = T + 1
+    print("Number of A's: " + str(A))  #Prints the total amount of A's
+    print("Number of C's: " + str(C))  #Prints the total amount of C's
+    print("Number of G's: " + str(G))  #Prints the total amount of G's
+    print("Number of T's: " + str(T))  #Prints the total amount of T's
 
 if __name__ == '__main__':
-    s = 'ACGT' 
-    s = 'ACGTCAGTCAGG'
-    s = 'ACG'
+    MolecularBio('ACGT')                                                                   #Should return 1A, 1C, 1G, and 1T
+    MolecularBio('AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC') #Should return 20A's, 12C's, 17G's, 21T's.
 
+    
+    
 #Project Euler Question 12 (Sudo Code)
 
 they input x which is number of divisors
